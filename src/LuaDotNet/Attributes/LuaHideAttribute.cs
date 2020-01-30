302edee965f6460e2df1b53043acfb12dc04e8dd
ivanbiljan <ssbiljan@gmail.com>
2020-01-30 22:31:38 +0100

    Add NLua attributes

---
+++ src/LuaDotNet/Attributes/LuaHideAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace LuaDotNet.Attributes
+{
+    /// <summary>
+    ///     Indicates that the marked element is hidden (inaccessible) from the Lua runtime.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Event | AttributeTargets.Field | AttributeTargets.Method |
+                    AttributeTargets.Property)]
+    [PublicAPI]
+    public sealed class LuaHideAttribute : Attribute
+    {
+    }
+}
