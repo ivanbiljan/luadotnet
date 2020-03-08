69ad723acf14bf9ab8ced76bd6303e426d042220
ivanbiljan <ssbiljan@gmail.com>
2020-03-08 20:39:04 +0100

    Add interactive cli project

---
+++ tests/LuaDotNet.Interactive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using LuaDotNet;
+
+namespace LuaDotNet.Interactive {
+    class Program {
+        static void Main(string[] args) {
+            using (var lua = new LuaContext()) {
+                var input = string.Empty;
+                do {
+                    Console.Write("> ");
+                    input = Console.ReadLine() ?? string.Empty;
+                    try {
+                        if (File.Exists(input)) {
+                            // run file
+                            Console.Write("Run file");
+                        }
+                        else {
+                            lua.DoString(input);
+                        }
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex);
+                    }
+                } while (!input.Equals("exit", StringComparison.InvariantCultureIgnoreCase));
+            }
+        }
+    }
+}
